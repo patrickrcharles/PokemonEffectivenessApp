eb04ce53a6c6fa87eb44f1eb39b84bd55867e603
Patrick Charles <patrickrcharles@gmail.com>
2025-11-26 12:34:04 -0600

    Add project files.

---
+++ Core/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonEffectivenessApp.Core.Models
+{
+    public record Pokemon(string Name, List<PokemonType> Types);
+}
---
+++ Core/Models/TypeRelations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonEffectivenessApp.Core.Models
+{
+    public record TypeRelations(
+        List<string> DoubleDamageTo,
+        List<string> HalfDamageTo,
+        List<string> NoDamageTo,
+        List<string> DoubleDamageFrom,
+        List<string> HalfDamageFrom,
+        List<string> NoDamageFrom
+    );
+
+}
