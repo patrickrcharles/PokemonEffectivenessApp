38bcae9c2de244fd4fa73ff435ed73c412d20ab5
Patrick Charles <patrickrcharles@gmail.com>
2025-11-29 14:57:33 -0600

    update namespace

---
+++ ApiClient/IPokeClient.cs
@@ -0,0 +1,11 @@
+﻿using PokemonEffectivenessApp.Models;
+
+namespace PokemonEffectivenessApp.ApiClient
+{
+    public interface IPokeApiClient
+    {
+        Task<PokemonDto?> GetPokemonAsync(string name);
+        Task<TypesDto?> GetTypeByUrlAsync(string url);
+    }
+
+}
