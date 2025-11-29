2172e8f5a0d433285f54ecf8fbd410edc4f7e54d
Patrick Charles <patrickrcharles@gmail.com>
2025-11-29 14:58:40 -0600

    add inline exception error message

---
+++ ApiClient/PokeClient.cs
@@ -0,0 +1,48 @@
+﻿using PokemonEffectivenessApp.ApiClient;
+using PokemonEffectivenessApp.Models;
+using System.Net.Http.Json;
+
+namespace PokemonEffectivenessApp.ApiClient
+{
+    public class PokeApiClient(HttpClient http) : IPokeApiClient
+    {
+        private readonly HttpClient _http = http;
+        private readonly string _baseUrl = "https://pokeapi.co/api/v2";
+
+        public async Task<PokemonDto?> GetPokemonAsync(string name)
+        {
+            var url = new Uri(_baseUrl + $"/pokemon/{name}");
+            try
+            {
+                var res = await _http.GetAsync(url);
+                if (!res.IsSuccessStatusCode)
+                    throw new HttpRequestException($"Failed to fetch Pokémon '{name}'. Status code: {res.StatusCode}");
+
+                var content = await res.Content.ReadFromJsonAsync<PokemonDto>();
+                return content;
+            }
+            catch (Exception ex) { 
+                Console.WriteLine($"Network Error : {ex.Message}");
+                return default;
+            }
+        }
+
+        public async Task<TypesDto?> GetTypeByUrlAsync(string url)
+        {
+            try
+            {
+                var res = await _http.GetAsync(url.Trim());
+                if (!res.IsSuccessStatusCode)
+                    throw new HttpRequestException($"Failed to fetch Type from URL '{url}', status code: {res.StatusCode}");
+
+                var content = await res.Content.ReadFromJsonAsync<TypesDto>();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Network Error : {ex.Message}");
+                return default;
+            }
+        }
+    }
+}
